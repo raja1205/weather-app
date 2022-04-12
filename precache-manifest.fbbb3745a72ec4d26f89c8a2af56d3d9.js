6f2223057fa82c281dcc7e481a4e98feff97b796
raja1205 <rajacool84@gmail.com>
2022-04-12 23:26:55 +0530

    Updates

---
+++ precache-manifest.fbbb3745a72ec4d26f89c8a2af56d3d9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "197f29755870aea14059",
+    "url": "/weather-app/static/css/main.46e32b53.chunk.css"
+  },
+  {
+    "revision": "197f29755870aea14059",
+    "url": "/weather-app/static/js/main.197f2975.chunk.js"
+  },
+  {
+    "revision": "8d9c99a4f19116c6d931",
+    "url": "/weather-app/static/js/runtime~main.8d9c99a4.js"
+  },
+  {
+    "revision": "39f529fce33af15616ab",
+    "url": "/weather-app/static/css/2.a0e833e8.chunk.css"
+  },
+  {
+    "revision": "39f529fce33af15616ab",
+    "url": "/weather-app/static/js/2.39f529fc.chunk.js"
+  },
+  {
+    "revision": "a8f9a350e36265c2fa9437b68869af24",
+    "url": "/weather-app/static/media/bgg.a8f9a350.jpg"
+  },
+  {
+    "revision": "ea66953c9badd175254c95f657656866",
+    "url": "/weather-app/static/media/bg.ea66953c.jpg"
+  },
+  {
+    "revision": "775ad03c53380bdcc3a97eb0a75d88fa",
+    "url": "/weather-app/index.html"
+  }
+];
